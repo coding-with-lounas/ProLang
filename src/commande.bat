1cff8556cbd4ea9d3ce098b53f139a39d6fe1b0b
coding-with-lounas <lounasidjr@gmail.com>
2026-04-06 03:47:05 +0100

    readme file change

--- src/commande.bat
+++ src/commande.bat
@@ -1,3 +1,6 @@
+@REM bison -d syntax.y
 flex lexical.l
 gcc lex.yy.c -o lexical_analyzer -lfl
+@REM gcc lex.yy.c syntax.tab.c -o compilateur -lfl -ly
 ./lexical_analyzer < source.txt
+@REM ./compilateur < source.txt
