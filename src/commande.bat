ff90b5c11a32f4a04ce7614b626701c3324f3f1c
coding-with-lounas <lounasidjr@gmail.com>
2026-04-26 16:40:32 +0100

    error test

--- src/commande.bat
+++ src/commande.bat
@@ -1,6 +1,6 @@
 bison -d syntax.y
 flex lexical.l
 gcc lex.yy.c syntax.tab.c TS.c -o compilateur -lfl
-@REM ./compilateur < final_test.txt
+@REM ./compilateur < final_test_error.txt
 
 ./compilateur < final.txt
